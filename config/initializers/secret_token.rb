62ff344221c234dc32d30bb112cc6482915f19d1
Sai Ponduru <sai_ponduru@yahoo.com>
2013-04-03 09:15:19 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DBNotes::Application.config.secret_token = '17527ed2c3e9137f6e114177e60ec2bbba993af3d1858136a4b642f6f5279ba3cb16a83ca777551de01ebd91a3426d50ee38cec39163b361224fa12688fd2048'
